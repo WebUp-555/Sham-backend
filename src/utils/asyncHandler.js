0f4811d9259382d20eb6da081e06820dbc9cce2e
WebUp-555 <designcore.dev@gmail.com>
2025-09-05 20:07:54 +0530

    all set up configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,13 +1,13 @@
-const aysncHandler=(requestHandler)=>{
-    (req,res,next)=>{
-        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
-    }
-}
+const asyncHandler = (requestHandler) => {
+   return (req, res, next) => {
+       Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+   };
+};
 
 
 
 
-export { aysncHandler }
+export { asyncHandler }
 
 
 
