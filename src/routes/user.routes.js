98177208e2802a4a7ab8253a89fcfbcae13f69b8
WebUp-555 <designcore.dev@gmail.com>
2025-09-16 20:43:55 +0530

    Added new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,10 @@
 import {Router} from "express";
-import {loginUser, registerUser,logoutUser,refreshAccessToken } from "../controllers/user.controller.js";
+     
+import {
+    loginUser, registerUser,logoutUser,refreshAccessToken ,changeCurrentPassword,getCurrentUser,updateAccountDetails, updateUserAvatar,updateUserCoverimage,getUSerChannelProfile,getWatchHistory} from "../controllers/user.controller.js";
 import { upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 const router= Router()
 
 router.route("/register").post(
@@ -23,4 +26,12 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverimage)
+router.route("/c/:username").get(verifyJWT,getUSerChannelProfile)
+router.route("/watchHistory").get(verifyJWT,getWatchHistory)
 export default router;
